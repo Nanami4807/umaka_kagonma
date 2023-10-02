ef6f0500d88ae9dc5914fe0bda52c9b466777220
22S030 <22S030@ca.harada-gakuen.ac.jp>
2023-10-02 12:19:33 +0900

    2023/10/2

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/dayjs'
+    //'@nuxtjs/axios'         //コメントアウトを外すとエラーになる
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    const { data } = await $axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://umaka.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'bsKimZKgVvPzOdGgGUxIJTx3g7COGcmPI4yE' }
+      }
+    )
+    return data
+  }
+}
+</script>
