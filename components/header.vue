c2d490b73311d64db0a95d5a24b2bd08c9114017
22S030 <22S030@ca.harada-gakuen.ac.jp>
2023-12-07 12:23:16 +0900

    デザインに基づいて変更

--- components/header.vue
+++ components/header.vue
@@ -1,18 +1,26 @@
 <template>
-    <div class="bg-indigo">
-        <div class="container ">
+    <div class="bg-brown">
+        <div class="container">
             <div class="row">
-                <div class="col-3">
-                    <button class="border-0 w-100 h-100 bg-blue rounded-0" @click="$router.push('/')"><picture><source srcset="../image/logo_triming.png" media="(min-width: 500px)" type="image/png"><img class="w-75 change-img" src="../image/logo.png"></picture></button>
+                <div class="col-2">
+                <p class="text-whtie">旗</p>
                 </div>
-                <div class="col-3 d-flex align-items-center">
-                    <button type="button" class="border-0 w-100  h-100 h-100 h4 bg-blue rounded-0 text-warning" @click="$router.push('/tiku')">地区</button>
-                </div>
-                <div class="col-3 d-flex align-items-center">
-                    <button type="button" class="border-0 w-100  h-100 h-100 h4 bg-blue rounded-0 text-warning" @click="$router.push('/Category')">カテゴリ</button>
-                </div>
-                <div class="col-3 d-flex align-items-center">
-                    <button type="button" class="border-0 w-100 h-100 h-100 h4 bg-blue rounded-0 text-warning" @click="$router.push('/osusume')">おすすめ</button>
+
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-3 d-flex align-items-center border-right my-4">
+                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">地区</button>
+                        </div>
+                        <div class="col-3 d-flex align-items-center border-right my-4">
+                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">カテゴリ</button>
+                        </div>
+                        <div class="col-3 d-flex align-items-center border-right my-4">
+                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/Category')">おすすめ</button>
+                        </div>
+                        <div class="col-3 d-flex align-items-center my-4 ">
+                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white" @click="$router.push('/osusume')">記事一覧</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
