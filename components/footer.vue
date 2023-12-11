aee5826a33e39e9a6e9fb16d35c38dbcac79e0b6
22S030 <22S030@ca.harada-gakuen.ac.jp>
2023-12-11 12:20:48 +0900

    ヘッダーとフッター

--- components/footer.vue
+++ components/footer.vue
@@ -1,25 +1,38 @@
 <template>
   <footer class="bg-brown">
     <div class="container">
-      <div class="row">
+      <div class="row pt-5">
 
-        <div class="col-md-4">
-          <button class="border-0 w-100 h-100 bg-brown rounded-0" @click="$router.push('/')"><img class="w-75 d-block mx-auto" src="../image/logo_triming.png"></button>
-        </div>
-
-        <div class="col-md-2 d-flex align-items-center">
+        <div class="col-md-3 d-flex align-items-center border-right my">
             <button type="button" class="border-0 w-100  h-100 h-100 h4 rounded-0 bg-brown text-white mb-0" @click="$router.push('/tiku')">地区</button>
         </div>
-        <div class="col-md-2 d-flex align-items-center">
+        <div class="col-md-3 d-flex align-items-center border-right my">
             <button type="button" class="border-0 w-100  h-100 h-100 h4 rounded-0 bg-brown text-white mb-0" @click="$router.push('/Category')">カテゴリ</button>
         </div>
-        <div class="col-md-2 d-flex align-items-center">
+        <div class="col-md-3 d-flex align-items-center border-right my">
             <button type="button" class="border-0 w-100 h-100 h-100 h4 rounded-0 bg-brown text-white mb-0" @click="$router.push('/osusume')">おすすめ</button>
         </div>
 
-        <div class="col-md-2 d-flex align-items-center">
+        <div class="col-md-3 d-flex align-items-center my">
             <button type="button" class="border-0 w-100 h-100 h-100 h4 rounded-0 bg-brown text-white mb-0" @click="$router.push('/pages/list/1')">記事一覧</button>
         </div>
+
+        <div class="col-md-2">
+        </div>
+
+        <div class="col-2">
+        </div>
+
+        <div class="col-md-4 col-8 text-center">
+          <img class="w-50" src="../image/logo.png">
+        </div>
+
+        <div class="col-md-2">
+        </div>
+
+        <div class="col-2">
+        </div>
+        
 
       </div>
     </div>
--- components/header.vue
+++ components/header.vue
@@ -1,30 +1,62 @@
 <template>
-    <div class="bg-brown">
-        <div class="container">
-            <div class="row">
-                <div class="col-2">
-                <p class="text-whtie">旗</p>
-                </div>
+<div>
+    <div id="header">
+        <div class="bg-brown">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                        <img class="w-100" src="../image/flag.png">
+                    </div>
 
-                <div class="col-10">
-                    <div class="row">
-                        <div class="col-3 d-flex align-items-center border-right my-4">
-                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">地区</button>
-                        </div>
-                        <div class="col-3 d-flex align-items-center border-right my-4">
-                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">カテゴリ</button>
-                        </div>
-                        <div class="col-3 d-flex align-items-center border-right my-4">
-                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/Category')">おすすめ</button>
-                        </div>
-                        <div class="col-3 d-flex align-items-center my-4 ">
-                            <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white" @click="$router.push('/osusume')">記事一覧</button>
+                    <div class="col-10">
+                        <div class="row">
+                            <div class="col-3 d-flex align-items-center border-right my-4">
+                                <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">地区</button>
+                            </div>
+                            <div class="col-3 d-flex align-items-center border-right my-4">
+                                <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/tiku')">カテゴリ</button>
+                            </div>
+                            <div class="col-3 d-flex align-items-center border-right my-4">
+                                <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white " @click="$router.push('/Category')">おすすめ</button>
+                            </div>
+                            <div class="col-3 d-flex align-items-center my-4 ">
+                                <button type="button" class="border-0 w-100 bg-brown h-100 h4  text-white" @click="$router.push('/osusume')">記事一覧</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
+    <div id="hamburger" class="">
+        <header class="header bg-brown">
+                <div class="logo ">
+                    <br>
+                </div>
+                <div class="nav">
+    
+                    <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+                    <input id="drawer_input" class="drawer_hidden" type="checkbox">
+            
+                    <!-- ハンバーガーアイコン -->
+                    <label for="drawer_input" class="drawer_open"><span></span></label>
+            
+                    <!-- メニュー -->
+                    <nav class="nav_content">
+                    <ul class="nav_list">
+                            <li class="nav_item"><NuxtLink to="/">TOP</NuxtLink></li>
+                            <li class="nav_item"><NuxtLink to="/tiku">地区</NuxtLink></li>
+                            <li class="nav_item"><NuxtLink to="/Category">カテゴリ</NuxtLink></li>
+                        <li class="nav_item"><NuxtLink to="/osusume">オススメ</NuxtLink></li>
+                    <li class="nav_item"><NuxtLink to="pages/list/1">記事一覧</NuxtLink></li>
+                    </ul>
+                    </nav>
+    
+                </div>
+        </header>
+    </div>
+</div>
 </template>
 <script>
     export default{
